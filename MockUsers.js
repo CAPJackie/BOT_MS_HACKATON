3417d1c21fad6e1bd54bb58de3b85faccc101d08
It7jzi9HyW <juan.ramirez-me@mail.escuelaing.edu.co>
2019-02-21 19:48:27 -0500

    Mock Data

---
+++ MockUsers.js
@@ -0,0 +1,21 @@
+var users = [{
+    name:"Juan David Ramirez Mendoza",
+    career:"Computer Software Engineer",
+    gpa:3.78,
+    }, 
+    {
+        name:"Juan Nicolas Gomez",
+        career:"Computer Software Engineer",
+        gpa:3.8,
+        }, {
+            name:"Camilo Ramirez",
+            career:"Industrial Engineer",
+            gpa:4.5,
+            }, {
+                name:"Felipe",
+                career:"Mechanical Engineer",
+                gpa:3.78,
+                }]
+
+
+exports.users = users;
